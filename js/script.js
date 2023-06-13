91a61b0da131e1401fb8fcef051e6be390f2469c
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-13 12:38:59 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//JS RESET
+console.log('JS OK')
+
+//ARRAY AUTOMOBILI
+const cars = [
+    {
+      marca: 'Alfa Romeo',
+      modello: 'Giulietta',
+      alimentazione: 'benzina',
+    },
+    {
+      marca: 'FIAT',
+      modello: 'Doblò',
+      alimentazione: 'diesel',
+    },
+    {
+      marca: 'FIAT',
+      modello: 'Panda',
+      alimentazione: 'benzina',
+    },
+    {
+      marca: 'FIAT',
+      modello: 'Fiorino',
+      alimentazione: 'metano',
+    },
+    {
+      marca: 'Ford',
+      modello: 'Fiesta',
+      alimentazione: 'diesel',
+    },
+    {
+      marca: 'Ford',
+      modello: 'Ka',
+      alimentazione: 'gpl',
+    },
+    {
+      marca: 'Smart',
+      modello: 'Smart',
+      alimentazione: 'elettrico',
+    },
+  ];
+
---
+++ steps.md
@@ -0,0 +1,14 @@
+# TRACCIA: 
+Crea un array composto da 10 automobili.
+Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+Infine stampa separatamente i 3 array.
+
+## MILESTONE 
+- CREO un array di oggetti (che elenchi marche, alimentazione e modello)
+- CREO un array dei veicoli a benzina
+    * usando FILTER 
+-  CREO un array dei veicoli a diesel
+    * usando FILTER 
+-  CREO un array dei veicoli restanti
+    * usando FILTER 
